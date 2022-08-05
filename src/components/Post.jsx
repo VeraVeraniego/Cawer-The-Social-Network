a4590981706efeacec4d7861ae934e600346bb4c
Brayan Vera <brayanvera@ravn.co>
2022-08-04 20:27:41 -0500

    style: styling Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 import palette from "../theme/global-styles";
 const PostContainer = styled.section`
+  min-width: 1011px;
   display: grid;
+  border: solid 1px #e1e1e1;
   grid-template:
     "profile title"
     "profile desc";
+  /* align-items: center; */
+  justify-content: left;
+  gap: 18px;
 `;
 const PostImage = styled.img`
   grid-area: profile;
@@ -12,20 +17,28 @@ const PostImage = styled.img`
   height: 99px;
   border-radius: 49.5px;
   background-color: ${palette.gray};
+  margin: 30px;
 `;
 const PostTitle = styled.h3`
   grid-area: title;
   font-size: 24px;
+  color: ${palette.green};
+  align-self: flex-end;
 `;
 const PostDescription = styled.p`
+  align-self: flex-start;
   grid-area: desc;
+  color: ${palette.green};
 `;
 export function Post() {
   return (
     <PostContainer>
-      <PostImage alt="Profile"></PostImage>
-      <PostTitle>this is a post</PostTitle>
-      <PostDescription>This is a paragraph</PostDescription>
+      <PostImage
+        alt="Profile"
+        src="https://freesvg.org/storage/img/thumb/abstract-user-flat-3.png"
+      ></PostImage>
+      <PostTitle>This is a post</PostTitle>
+      <PostDescription>This is a long paragraph</PostDescription>
     </PostContainer>
   );
 }
