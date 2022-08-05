1397b168c0c0313f2d7298f1e12e06b7f0da3734
Brayan Vera <brayanvera@ravn.co>
2022-08-04 21:17:27 -0500

    feat: add single post view (progress) and styles to it

--- src/pages/PostDetails.tsx
+++ src/pages/PostDetails.tsx
@@ -1,11 +1,31 @@
+import styled from "styled-components";
 import { SideBar } from "../components/SideBar";
 import { GlobalStyle } from "../theme/global-styles";
 
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const PostsAndCommentsContainer = styled.article``;
+const FullPostAndAuthorInfoContainer = styled.section``;
+const FullPost = styled.section``;
+const AuthorInfo = styled.section``;
+const CommentsContainer = styled.div``;
+const Comment = styled.div``;
 export function PostDetails() {
   return (
-    <>
+    <PageContainer>
       <GlobalStyle />
       <SideBar />
-    </>
+      <PostsAndCommentsContainer>
+        <FullPostAndAuthorInfoContainer>
+          <FullPost></FullPost>
+          <AuthorInfo></AuthorInfo>
+        </FullPostAndAuthorInfoContainer>
+        <CommentsContainer>
+          <Comment />
+        </CommentsContainer>
+      </PostsAndCommentsContainer>
+    </PageContainer>
   );
 }
