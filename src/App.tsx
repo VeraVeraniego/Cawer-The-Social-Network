e4b25babbc6523ac2cf3ea57800a26e0f4c3a949
Brayan Vera <BrayanVera@ravn.co>
2022-08-05 22:19:38 -0500

    feat: add NotFound Page to bad paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+import { NotFound } from "./pages/NotFound";
 import { PostDetails } from "./pages/PostDetails";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/login" element={<Login />}></Route>
       <Route index element={<Home />}></Route>
       <Route path="/post" element={<PostDetails />}></Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
