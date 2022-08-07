09a7feab027e8f9b636b9bc4becd0e721778c14b
Brayan Vera <BrayanVera@ravn.co>
2022-08-07 06:55:03 -0500

    feat: add constants for api calls

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,8 @@
+const API_URL = "https://jsonplaceholder.typicode.com";
 export const JSONPLACEHOLDERS_API = {
-  USERS: "https://jsonplaceholder.typicode.com/users",
+  USERS: `${API_URL}/users`,
+  USER_POSTS_ID: `${API_URL}/posts?userId=`,
+  ALL_POSTS: `${API_URL}/posts`,
+  PHOTOS: `${API_URL}/photos`,
+  COMMENTS: `${API_URL}/comments?postId=`,
 };
