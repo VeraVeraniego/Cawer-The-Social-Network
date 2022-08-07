33d64d063d90a375837e863b6e51339b60bea82b
Brayan Vera <BrayanVera@ravn.co>
2022-08-07 06:39:34 -0500

    feat: add home page logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,17 @@ import styled from "styled-components";
 // import palette from "../theme/global-styles";
 // import { Link } from "react-router-dom";
 import { Post } from "../components/Post";
-import { SideBar } from "../components/SideBar";
+import { getAllPosts, getUserPosts } from "../utils/api";
+import { useEffect, useState } from "react";
+import { IUser } from "../interfaces/IUser";
+import { IPost } from "../interfaces/IPost";
+import { IPhotos } from "../interfaces/IPhotos";
+import { JSONPLACEHOLDERS_API } from "../utils/constants";
 
 const PageContainer = styled.div`
   display: flex;
   /* flex-direction: column; */
 `;
-
 const PostsContainer = styled.main`
   display: flex;
   flex-direction: column;
@@ -17,16 +21,48 @@ const PostsContainer = styled.main`
   margin: 97px 193px 97px 596px;
 `;
 
-export function Home() {
+export function Home({ path }: { path: "home" | "feed" }) {
+  const [user] = useState<IUser>(JSON.parse(localStorage.getItem("userAuth")!));
+  const [posts, setPosts] = useState<IPost[]>();
+  // const [photos, setPhotos] = useState<IPhotos[]>();
+
+  const id = user.id;
+
+  useEffect(() => {
+    async function getData() {
+      const postsData = await getUserPosts(id);
+      const allPostsData = await getAllPosts();
+      if (path === "home") {
+        console.log("home");
+        setPosts(await postsData.data);
+      }
+      if (path === "feed") {
+        console.log("feed");
+
+        setPosts(await allPostsData.data);
+      }
+      console.log(posts);
+      // setPhotos(photosData.data);
+    }
+    getData();
+  }, [path]);
+
   return (
     <PageContainer>
       <GlobalStyle />
-      {/* <SideBar /> */}
       <PostsContainer>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {posts?.map((ele) => {
+          return (
+            <Post
+              key={ele.id}
+              title={ele.title}
+              body={ele.body}
+              // img={JSONPLACEHOLDERS_API.PHOTOS}
+              // since photos api won't work for me I'm using 14 images I downloaded from internet and loop them
+              img={`./imgs/${ele.id > 14 ? ele.id % 14 : ele.id}.jpg`}
+            />
+          );
+        })}
       </PostsContainer>
     </PageContainer>
   );
