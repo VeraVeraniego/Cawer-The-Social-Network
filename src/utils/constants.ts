6740c1f9442463d30cfd25e074fbaa6120156753
Brayan Vera <BrayanVera@ravn.co>
2022-08-07 12:48:20 -0500

    feat: create a new constant of apis to get photos

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,12 @@ export const JSONPLACEHOLDERS_API = {
   USERS: `${API_URL}/users`,
   USER_POSTS_ID: `${API_URL}/posts?userId=`,
   ALL_POSTS: `${API_URL}/posts`,
-  PHOTOS: `${API_URL}/photos`,
   COMMENTS: `${API_URL}/comments?postId=`,
+};
+
+export const PHOTOS_API = {
+  USER_PHOTOS: (id: number) =>
+    `https://rickandmortyapi.com/api/character/avatar/${id}.jpeg`,
+  USER_PHOTOS2: `https://rickandmortyapi.com/api/character/`,
+  PHOTOS: (id: number) => `https://picsum.photos/id/10${id}/300/200`,
 };
