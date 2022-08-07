3399e8c2362b2960c90f59683a603becf0bc7a30
Brayan Vera <BrayanVera@ravn.co>
2022-08-07 01:45:38 -0500

    feat: add subroutes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { SideBar } from "./components/SideBar";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { NotFound } from "./pages/NotFound";
 import { PostDetails } from "./pages/PostDetails";
 
 function App() {
+  const [authenticate, setAuthenticate] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const auth = localStorage.getItem("userAuth");
+    auth && JSON.parse(auth) ? setAuthenticate(true) : setAuthenticate(false);
+  }, []);
+
   return (
     <Routes>
-      <Route path="/login" element={<Login />}></Route>
-      <Route index element={<Home />}></Route>
-      <Route path="/post" element={<PostDetails />}></Route>
+      <Route
+        path="/login"
+        element={<Login authenticate={() => setAuthenticate(true)} />}
+      ></Route>
+      <Route path="/" element={<SideBar />}>
+        <Route path="home" element={<Home />}></Route>
+        <Route path="post" element={<PostDetails />}></Route>
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
