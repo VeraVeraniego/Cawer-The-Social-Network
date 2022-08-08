a9ef32c3a56c21365bad57b3a5d863468c3bcc07
Brayan Vera <BrayanVera@ravn.co>
2022-08-07 21:38:24 -0500

    feat: add api calls to file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,16 +1,14 @@
 import axios from "axios";
 import { JSONPLACEHOLDERS_API } from "./constants";
-interface IApiResponse {
-  data?: Array<any>;
+export interface IApiResponse {
+  data?: Array<any> | any;
   error?: unknown;
 }
 
-export const getUserPosts = async (id: number) => {
+export const getUsers = async () => {
   const apiResponse: IApiResponse = {};
   try {
-    const response = await axios.get(
-      `${JSONPLACEHOLDERS_API.USER_POSTS_ID}${id}`
-    );
+    const response = await axios.get(`${JSONPLACEHOLDERS_API.USERS}`);
     apiResponse.data = response.data;
   } catch (error) {
     apiResponse.error = (error as Error).message;
@@ -18,10 +16,12 @@ export const getUserPosts = async (id: number) => {
   return apiResponse;
 };
 
-export const getUsers = async () => {
+export const getUserPosts = async (id: number) => {
   const apiResponse: IApiResponse = {};
   try {
-    const response = await axios.get(`${JSONPLACEHOLDERS_API.USERS}`);
+    const response = await axios.get(
+      `${JSONPLACEHOLDERS_API.USER_POSTS_BY_ID(id)}`
+    );
     apiResponse.data = response.data;
   } catch (error) {
     apiResponse.error = (error as Error).message;
@@ -40,12 +40,34 @@ export const getAllPosts = async () => {
   return apiResponse;
 };
 
-export const getPostComments = async (postId: string | undefined) => {
+export const getPostCommentsByPostId = async (postId: string | undefined) => {
   const apiResponse: IApiResponse = {};
   try {
     const response = await axios.get(
-      `${JSONPLACEHOLDERS_API.COMMENTS}${postId}`
+      `${JSONPLACEHOLDERS_API.COMMENTS(postId)}`
     );
+    apiResponse.data = response.data;
+  } catch (error) {
+    apiResponse.error = (error as Error).message;
+  }
+  return apiResponse;
+};
+
+export const getPostById = async (id: number) => {
+  const apiResponse: IApiResponse = {};
+  try {
+    const response = await axios.get(`${JSONPLACEHOLDERS_API.POST_BY_ID(id)}`);
+    apiResponse.data = response.data;
+  } catch (error) {
+    apiResponse.error = (error as Error).message;
+  }
+  return apiResponse;
+};
+
+export const getUserById = async (id: number) => {
+  const apiResponse: IApiResponse = {};
+  try {
+    const response = await axios.get(`${JSONPLACEHOLDERS_API.USER_BY_ID(id)}`);
     apiResponse.data = response.data;
   } catch (error) {
     apiResponse.error = (error as Error).message;
